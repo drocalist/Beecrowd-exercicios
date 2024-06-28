3d787889313f6c111c3f34cc6807c8f11b45a92b
drocalist <166173217+drocalist@users.noreply.github.com>
2024-06-28 14:03:26 -0300

    Add files via upload

---
+++ 1078.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+for i in range(1):
+    linha1 = n * 1
+    linha2 = n * 2
+    linha3 = n * 3
+    linha4 = n * 4
+    linha5 = n * 5
+    linha6 = n * 6
+    linha7 = n * 7
+    linha8 = n * 8
+    linha9 = n * 9
+    linha10 = n * 10
+
+    print("1 x {} = {}".format(n,linha1))
+    print("2 x {} = {}".format(n, linha2))
+    print("3 x {} = {}".format(n, linha3))
+    print("4 x {} = {}".format(n, linha4))
+    print("5 x {} = {}".format(n, linha5))
+    print("6 x {} = {}".format(n, linha6))
+    print("7 x {} = {}".format(n, linha7))
+    print("8 x {} = {}".format(n, linha8))
+    print("9 x {} = {}".format(n, linha9))
+    print("10 x {} = {}".format(n, linha10))
+
